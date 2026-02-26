575e905bfee5a68e15ad9b9d38ddbf1d3bb901e1
benji <benjamin.wolff@digitecgalaxus.ch>
2026-02-27 00:43:04 +0100

    replace version at right time

--- recipes/emsdk/all/conanfile.py
+++ recipes/emsdk/all/conanfile.py
@@ -50,17 +50,17 @@ class EmSDKConan(ConanFile):
         with chdir(self, self.source_folder):
             emsdk = "emsdk.bat" if self.settings_build.os == "Windows" else "./emsdk"
             self.run(f"{emsdk} install latest")
-            replace_in_file(
-                    self,
-                    file_path=emdawnwebgpupy,
-                    search="_VERSION = 'v20251002.162335'",
-                    replace="_VERSION = 'v20260219.200501'")
-            replace_in_file(
-                    self,
-                    file_path=emdawnwebgpupy,
-                    search="SHA512 = 'ed15672c2c495a77c764929e6979f4e155bf8b9c46dee5b0f234f3208a708bc2b846d89eef345b725d03454b56d549531f48fc84ff2afe7627d14115893b0fb0'",
-                    replace="SHA512 = '67f64ae3263e2111ca5d71b0ea69f0fbf42a0a7cd40ada66c3975e031f9af07411e1390a8b52fec08d563a99612ca6a3b3f75115191253d41fca18b8f3494f9c'")
             self.run(f"{emsdk} activate latest")
+        replace_in_file(
+                self,
+                file_path=emdawnwebgpupy,
+                search="_VERSION = 'v20251002.162335'",
+                replace="_VERSION = 'v20260219.200501'")
+        replace_in_file(
+                self,
+                file_path=emdawnwebgpupy,
+                search="SHA512 = 'ed15672c2c495a77c764929e6979f4e155bf8b9c46dee5b0f234f3208a708bc2b846d89eef345b725d03454b56d549531f48fc84ff2afe7627d14115893b0fb0'",
+                replace="SHA512 = '67f64ae3263e2111ca5d71b0ea69f0fbf42a0a7cd40ada66c3975e031f9af07411e1390a8b52fec08d563a99612ca6a3b3f75115191253d41fca18b8f3494f9c'")
 
     def package(self):
         copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
